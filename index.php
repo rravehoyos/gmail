74a0ab02fad02a6d02d4521e6c4eddba1e9f9bb1
rravehoyos <67196031+rravehoyos@users.noreply.github.com>
2020-10-16 12:47:07 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gmail</title>
+</head>
+<body>
+	<form method="post">
+		<input type="text" name="name" placeholder="nombre" required="">
+		<input type="email" name="email"placeholder="correo"required="">
+		<input type="text" name="asunto"placeholder="asunto"required="">
+		<textarea placeholder="correo" name="msg"required=""></textarea>
+		<input type="submit" name="enviar">
+
+	
+
+	</form>
+		
+	<?php 
+	if(isset($_POST['enviar'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$msg=$_POST['msg'];
+		$asunto=$_POST['asunto'];
+		$header="From: rravehoyos@gmail.com"."\r\n";
+		$header.="X-Mailer:PHP/".phpversion();
+		mail($email,$asunto,$msg,$header);
+
+	}
+
+	 ?>
+
+
+</body>
+</html>
